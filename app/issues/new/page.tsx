53f4a01b0e9fc356d4969760d0b4b22fd083a87f
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-07 11:00:03 +0200

    disable ssr for loading markdown editor

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -3,14 +3,18 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Callout, TextField } from '@radix-ui/themes'
 import axios from 'axios'
 import 'easymde/dist/easymde.min.css'
+import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 import { Controller, SubmitHandler, useForm } from 'react-hook-form'
-import SimpleMDE from 'react-simplemde-editor'
 import z from 'zod'
 import ErrorMessage from '../../components/ErrorMessage'
 import Spinner from '../../components/Spinner'
 import { createIssueSchema } from '../../validationSchemas/createIssueSchema'
+
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), {
+  ssr: false,
+})
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
