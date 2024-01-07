c4bd9ebbd41659f88ec454f35fcec8a9d40614d4
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-07 15:50:47 +0200

    handle errors

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -2,42 +2,66 @@
 import { AlertDialog, Button, Flex } from '@radix-ui/themes'
 import axios from 'axios'
 import { useRouter } from 'next/navigation'
+import { useState } from 'react'
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter()
+  const [error, setError] = useState(false)
 
-  const handleClick = async () => {
-    await axios.delete(`/api/issues/${issueId}`)
-    router.push('/issues')
-    router.refresh()
+  const deleteIssue = async () => {
+    try {
+      await axios.delete(`/api/issues/${issueId}`)
+      router.push('/issues')
+      router.refresh()
+    } catch (error) {
+      setError(true)
+    }
   }
 
   return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color='red'>Delete Issue</Button>
-      </AlertDialog.Trigger>
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color='red'>Delete Issue</Button>
+        </AlertDialog.Trigger>
 
-      <AlertDialog.Content style={{ maxWidth: 450 }}>
-        <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
-        <AlertDialog.Description size='2' mb='4'>
-          Are you sure you want to delete this issue?
-        </AlertDialog.Description>
+        <AlertDialog.Content style={{ maxWidth: 450 }}>
+          <AlertDialog.Title>Confirm Deletion</AlertDialog.Title>
+          <AlertDialog.Description size='2' mb='4'>
+            Are you sure you want to delete this issue?
+          </AlertDialog.Description>
 
-        <Flex gap='3' mt='4' justify='end'>
-          <AlertDialog.Action>
-            <Button color='red' onClick={handleClick}>
-              Delete Issue
-            </Button>
-          </AlertDialog.Action>
-          <AlertDialog.Cancel>
-            <Button variant='soft' color='gray'>
-              Cancel
-            </Button>
-          </AlertDialog.Cancel>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+          <Flex gap='3' mt='4' justify='end'>
+            <AlertDialog.Action>
+              <Button color='red' onClick={deleteIssue}>
+                Delete Issue
+              </Button>
+            </AlertDialog.Action>
+            <AlertDialog.Cancel>
+              <Button variant='soft' color='gray'>
+                Cancel
+              </Button>
+            </AlertDialog.Cancel>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content style={{ maxWidth: 450 }}>
+          <AlertDialog.Title>Error</AlertDialog.Title>
+          <AlertDialog.Description size='2' mb='4'>
+            This issue could not be deleted
+          </AlertDialog.Description>
+          <Button
+            variant='soft'
+            color='gray'
+            mt='2'
+            onClick={() => setError(false)}
+          >
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   )
 }
 
