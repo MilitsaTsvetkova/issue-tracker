354c38e7fb8274e351f036c3d1049f4d0436b82c
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-27 14:36:15 +0200

    show toast notifications

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -4,6 +4,7 @@ import { Select } from '@radix-ui/themes'
 import axios from 'axios'
 import { useQuery } from 'react-query'
 import Skeleton from '@/app/components/Skeleton'
+import toast, { Toaster } from 'react-hot-toast'
 
 const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   const {
@@ -24,27 +25,34 @@ const AssigneeSelect = ({ issue }: { issue: Issue }) => {
   }
 
   return (
-    <Select.Root
-      defaultValue={issue.assignedToUserId || ''}
-      onValueChange={(userId) => {
-        axios.patch(`/api/issues/${issue.id}`, {
-          assignedToUserId: userId === 'unassigned' ? null : userId,
-        })
-      }}
-    >
-      <Select.Trigger placeholder='Assign...'></Select.Trigger>
-      <Select.Content>
-        <Select.Group>
-          <Select.Label>Suggestions</Select.Label>
-          <Select.Item value='unassigned'>Unassigned</Select.Item>
-          {users?.map((user) => (
-            <Select.Item key={user.id} value={user.id}>
-              {user.name}
-            </Select.Item>
-          ))}
-        </Select.Group>
-      </Select.Content>
-    </Select.Root>
+    <>
+      <Toaster />
+      <Select.Root
+        defaultValue={issue.assignedToUserId || ''}
+        onValueChange={(userId) => {
+          axios
+            .patch(`/api/issues/${issue.id}`, {
+              assignedToUserId: userId === 'unassigned' ? null : userId,
+            })
+            .catch(() => {
+              toast.error('Changes could not be saved')
+            })
+        }}
+      >
+        <Select.Trigger placeholder='Assign...'></Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            <Select.Label>Suggestions</Select.Label>
+            <Select.Item value='unassigned'>Unassigned</Select.Item>
+            {users?.map((user) => (
+              <Select.Item key={user.id} value={user.id}>
+                {user.name}
+              </Select.Item>
+            ))}
+          </Select.Group>
+        </Select.Content>
+      </Select.Root>
+    </>
   )
 }
 
