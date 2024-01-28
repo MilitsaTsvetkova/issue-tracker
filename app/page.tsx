f579120d62f2e239bbcca19814c34bf6322f44dd
militsa-tsvetkova <militsa.tsvetkova@ft.com>
2024-01-28 10:36:37 +0200

    layout the dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import prisma from '../prisma/client'
 import IssueChart from './IssueChart'
 import IssueSummary from './IssueSummary'
 import LatestIssues from './LatestIssues'
+import { Flex, Grid } from '@radix-ui/themes'
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: Status.OPEN } })
@@ -11,10 +12,12 @@ export default async function Home() {
     where: { status: Status.IN_PROGRESS },
   })
   return (
-    <>
-      <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+    <Grid columns={{ initial: '1', md: '2' }} gap='5'>
+      <Flex direction='column' gap='5'>
+        <IssueSummary open={open} closed={closed} inProgress={inProgress} />
+        <IssueChart open={open} closed={closed} inProgress={inProgress} />
+      </Flex>
       <LatestIssues />
-      <IssueChart open={open} closed={closed} inProgress={inProgress} />
-    </>
+    </Grid>
   )
 }
